031a5c7511a6b69b367db58c7cc295b3d0bc2845
kmularise <kmula222@gmail.com>
2023-09-27 01:37:51 +0900

    fix: encryptor 설정 변수 수정(#1)

--- src/main/java/com/flab/tabling/global/config/SecurityConfig.java
+++ src/main/java/com/flab/tabling/global/config/SecurityConfig.java
@@ -1,9 +1,10 @@
 package com.flab.tabling.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.security.crypto.encrypt.BytesEncryptor;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,18 +15,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @Bean: 스프링 컨테이너에 의해 관리되는 빈 생성
  */
 
+@PropertySource("classpath:application.yml")
 @Configuration
 public class SecurityConfig {
+	@Value("${bytes-encryptor-password}")
+	private String encryptorPassword;
+	@Value("${bytes-encryptor-salt}")
+	private String encryptorSalt;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	/**
-	 * BytesEncryptor password, salt 설정은 추후에 시크릿 변수로 수정할 필요 있음
+	 * TODO : application.yml에서  encryptor password 및 salt 시크릿 변수로 관리
 	 */
 	@Bean
 	public BytesEncryptor bytesEncryptor() {
-		return Encryptors.stronger("This is a password!", "0123456f");
+		return Encryptors.stronger(encryptorPassword, encryptorSalt);
 	}
 }
