02fde7665aa2648eb10a5a18beda1f32c1e49436
kmularise <kmula222@gmail.com>
2023-10-08 00:34:21 +0900

    test: exception handling 추가(#7)

--- src/test/java/com/flab/tabling/member/controller/MemberControllerTest.java
+++ src/test/java/com/flab/tabling/member/controller/MemberControllerTest.java
@@ -5,20 +5,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flab.tabling.global.config.StringGenerateFixture;
+import com.flab.tabling.global.exception.ErrorResponse;
+import com.flab.tabling.global.exception.GlobalExceptionAdvice;
 import com.flab.tabling.member.domain.RoleType;
 import com.flab.tabling.member.dto.MemberAddDto;
 import com.flab.tabling.member.service.MemberRegisterService;
@@ -31,18 +34,23 @@ import com.flab.tabling.member.service.MemberRegisterService;
  * @AutoConfigureMockMvc : addFilters = false로 해서 Filter 없이 MockMvc auto-configuration을 설정했다.
  */
 
-@WebMvcTest(MemberController.class)
-@MockBean(JpaMetamodelMappingContext.class)
-@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
 class MemberControllerTest {
 	private final String postURI = "/members";
 
-	@MockBean
+	@InjectMocks
+	private MemberController memberController;
+	@Mock
 	private MemberRegisterService memberRegisterService;
-
-	@Autowired
 	private MockMvc mvc;
 	ObjectMapper objectMapper = new ObjectMapper();
+
+	@BeforeEach
+	void init() {
+		mvc = MockMvcBuilders.standaloneSetup(memberController)
+			.setControllerAdvice(new GlobalExceptionAdvice())
+			.build();
+	}
 
 	@DisplayName("회원가입")
 	@Test
@@ -82,4 +90,38 @@ class MemberControllerTest {
 		Assertions.assertThat(memberResponseDtoResult).usingRecursiveComparison().isEqualTo(memberResponseDto);
 	}
 
+	@DisplayName("name 30자 이상 시 예외 발생")
+	@Test
+	void checkInvalidPassword() throws Exception {
+		//given
+		String name = StringGenerateFixture.makeByNumbersAndAlphabets(32);
+		String password = StringGenerateFixture.makeByNumbersAndAlphabets(10);
+		RoleType roleType = RoleType.CUSTOMER;
+		String email = StringGenerateFixture.makeEmail(8);
+
+		MemberAddDto.Request memberRequestDto = MemberAddDto.Request
+			.builder()
+			.name(name)
+			.password(password)
+			.email(email)
+			.roleType(roleType)
+			.build();
+		ErrorResponse errorResponse = ErrorResponse
+			.builder()
+			.code("INVALID_PARAMETER")
+			.message("name is invalid")
+			.build();
+
+		//when
+		ResultActions resultActions = mvc.perform(
+			post(postURI)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(memberRequestDto)));
+		//then
+		MockHttpServletResponse response = resultActions.andExpect(status().isBadRequest())
+			.andReturn().getResponse();
+		ErrorResponse memberResponseDtoResult = objectMapper.readValue(response.getContentAsString(),
+			ErrorResponse.class);
+		Assertions.assertThat(memberResponseDtoResult).usingRecursiveComparison().isEqualTo(errorResponse);
+	}
 }
