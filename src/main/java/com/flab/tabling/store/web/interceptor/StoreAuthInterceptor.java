ff6a380063d679d81618f96dbe27dcde4b85b39f
jhl221123 <jhl221123@naver.com>
2023-10-09 22:32:34 +0900

    feat: 식당 기능 접근권한 검증 기능 추가(#12)
    - GET 요청은 검증 대상에서 제외
    - 세션 회원의 접근권한을 검증

---
+++ src/main/java/com/flab/tabling/store/web/interceptor/StoreAuthInterceptor.java
@@ -0,0 +1,50 @@
+package com.flab.tabling.store.web.interceptor;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.flab.tabling.member.service.RoleTypeCheckService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class StoreAuthInterceptor implements HandlerInterceptor {
+	private final RoleTypeCheckService roleTypeCheckService;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		String httpMethod = request.getMethod();
+		if (httpMethod.equals(HttpMethod.GET.name())) {
+			return true;
+		}
+
+		HttpSession session = request.getSession();
+		loginValidation(session);
+		memberRoleTypeValidation(session);
+		return true;
+	}
+
+	private void loginValidation(HttpSession session) {
+		if (session == null || session.getAttribute("LOGIN_SESSION") == null) { // TODO: 2023-10-07 로그인 기능 추가 후 세션 이름 교체
+			throw new RuntimeException("NO_SESSION"); // TODO: 2023-10-04 커스텀 인증 예외로 교체 필요
+		}
+	}
+
+	private void memberRoleTypeValidation(HttpSession session) {
+		Long sessionMemberId = getSessionMemberId(session);
+		boolean isSeller = roleTypeCheckService.isSeller(sessionMemberId);
+		if (!isSeller) {
+			throw new RuntimeException("INCORRECT_MEMBER_ROLE_TYPE"); // TODO: 2023-10-06 잘못된 회원 타입을 명시하는 커스텀 예외로 교체
+		}
+	}
+
+	private Long getSessionMemberId(HttpSession session) {
+		Object sessionValue = session.getAttribute("LOGIN_SESSION"); // TODO: 2023-10-07 로그인 기능 추가 후 세션 이름 교체
+		return Long.valueOf(sessionValue.toString());
+	}
+}
