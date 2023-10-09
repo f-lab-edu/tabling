55b8418f101ca60554e35686401023c866b02a5f
jhl221123 <jhl221123@naver.com>
2023-10-09 23:02:20 +0900

    feat: 식당 엔티티 생성(#12)

---
+++ src/main/java/com/flab/tabling/store/domain/Store.java
@@ -0,0 +1,44 @@
+package com.flab.tabling.store.domain;
+
+import com.flab.tabling.member.domain.Member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*
+@ManyToOne: 다대일 연관관계 지정
+@JoinColumn: 연관관계의 주인, 조인 컬럼 설정
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+	private String name;
+	private String category;
+	private String description;
+	private Integer maxWaitingCount;
+
+	@Builder
+	public Store(Member member, String name, String category, String description, Integer maxWaitingCount) {
+		this.member = member;
+		this.name = name;
+		this.category = category;
+		this.description = description;
+		this.maxWaitingCount = maxWaitingCount;
+	}
+}
