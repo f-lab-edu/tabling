46ed0f5d27d4ea25633eb44b1f51471f1c9f8e29
kmularise <kmula222@gmail.com>
2023-10-25 15:26:40 +0900

    fix: JUnit 리플렉션 위해 추가(#28)

--- src/main/java/com/flab/tabling/global/exception/ErrorResponse.java
+++ src/main/java/com/flab/tabling/global/exception/ErrorResponse.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorResponse {
 	private ErrorCode code;
 	private String message;
