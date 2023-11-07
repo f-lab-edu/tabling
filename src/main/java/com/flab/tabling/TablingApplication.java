0a4fd74304f094d6116b75f7b4d73b56b12640d1
kmularise <kmula222@gmail.com>
2023-11-07 13:58:32 +0900

    fix: auditing을 위한 설정 추가(#36)

--- src/main/java/com/flab/tabling/TablingApplication.java
+++ src/main/java/com/flab/tabling/TablingApplication.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * 1) @ComponentScan : @Component 어노테이션이 포함된 어노테이션을 스캔하여 빈으로 등록
  * 2) @EnableAutoConfiguration : 사전에 정의한 라이브러리들을 특정 조건이 만족될 경우 빈으로 등록
  */
-@EnableJpaAuditing
 @SpringBootApplication
 public class TablingApplication {
 	public static void main(String[] args) {
