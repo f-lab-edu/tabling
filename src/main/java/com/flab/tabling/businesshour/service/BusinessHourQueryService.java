4e660df7854fa64c83039907c532e9dccd42877f
jhl221123 <jhl221123@naver.com>
2023-11-13 01:37:01 +0900

    refactor: 반환 타입 수정(#40)

--- src/main/java/com/flab/tabling/businesshour/service/BusinessHourQueryService.java
+++ src/main/java/com/flab/tabling/businesshour/service/BusinessHourQueryService.java
@@ -3,5 +3,5 @@ package com.flab.tabling.businesshour.service;
 import java.time.LocalDateTime;
 
 public interface BusinessHourQueryService {
-	public Boolean isBusinessHour(Long storeId, LocalDateTime requestTime);
+	boolean isBusinessHour(Long storeId, LocalDateTime requestTime);
 }
