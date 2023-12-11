fbb776ac5d918b99e3daa3c0232a2d9fd342e836
jhl221123 <jhl221123@naver.com>
2023-12-11 23:11:26 +0900

    fix: audit 적용 누락 수정(#50)

--- src/main/java/com/flab/tabling/store/domain/Store.java
+++ src/main/java/com/flab/tabling/store/domain/Store.java
@@ -1,5 +1,6 @@
 package com.flab.tabling.store.domain;
 
+import com.flab.tabling.global.audit.BaseTime;
 import com.flab.tabling.member.domain.Member;
 
 import jakarta.persistence.Entity;
@@ -23,7 +24,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Store {
+public class Store extends BaseTime {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
