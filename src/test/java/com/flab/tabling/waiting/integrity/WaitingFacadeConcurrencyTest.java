ea543872a68587ecfccece76b39b0ca247efc15a
이준혁 <jhl221123@naver.com>
2024-09-03 15:10:44 +0900

    test: 대기 신청 기능 동시성 테스트 수정(#78)

--- src/test/java/com/flab/tabling/waiting/integrity/WaitingFacadeConcurrencyTest.java
+++ src/test/java/com/flab/tabling/waiting/integrity/WaitingFacadeConcurrencyTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,13 +23,12 @@ import com.flab.tabling.waiting.repository.WaitingRepository;
 import com.flab.tabling.waiting.service.WaitingService;
 
 import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
 
-/*
-@Commit: @Transactional 이 적용된 테스트 코드를 커밋할 수 있습니다.
- */
+@Slf4j
 @SpringBootTest
 public class WaitingFacadeConcurrencyTest {
-	static final int WAITING_SAMPLE_DATA_SIZE = 11;
+
 	@Autowired
 	WaitingService waitingService;
 	@Autowired
@@ -45,32 +43,29 @@ public class WaitingFacadeConcurrencyTest {
 	EntityManager entityManager;
 	@MockBean
 	LoginMemberAuditorAware loginMemberAuditorAware;
-	Long idx = 8L;
+	private Long memberIdx = 2L;
 
 	@Test
-	@Transactional(isolation = Isolation.READ_COMMITTED)
-	@Commit
+	@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 	@DisplayName("다수의 사용자가 동시에 대기 요청해도 최대 대기 가능 인원수를 초과하지 않는다.")
 	void concurrencyTest() throws InterruptedException {
 		//given
 		Store store = storeRepository.findById(1L).get();
 
 		//when
-		executeConcurrentRequests();
-		Thread.sleep(1000);
-		entityManager.flush();
-		entityManager.clear();
+		executeConcurrentRequests(store.getId());
 
 		//then
 		int countAfterConcurrentRequests = waitingRepository.countByStoreAndStatus(store, ONGOING);
 		assertThat(countAfterConcurrentRequests).isEqualTo(store.getMaxWaitingCount());
-		entityManager.createNativeQuery("delete from waiting where id > " + WAITING_SAMPLE_DATA_SIZE).executeUpdate();
+		entityManager.createNativeQuery("delete from waiting where id > " + 1L).executeUpdate();
 	}
 
-	private void executeConcurrentRequests() {
+	private void executeConcurrentRequests(Long storeId) throws InterruptedException {
 		ExecutorService executorService = Executors.newFixedThreadPool(10);
 		for (int i = 0; i < 8; i++) {
-			executorService.execute(() -> waitingFacade.add(1L, idx++, 3));
+			executorService.execute(() -> waitingFacade.add(storeId, memberIdx++, 3));
 		}
+		Thread.sleep(1000);
 	}
 }
