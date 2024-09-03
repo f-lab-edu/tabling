491268c4daa3dc8e6d8612acb51d3a5094eb5290
이준혁 <jhl221123@naver.com>
2024-09-03 15:06:21 +0900

    chore: 동시성 테스트를 위한 데이터베이스 설정(#78)

---
+++ src/test/resources/data.sql
@@ -0,0 +1,21 @@
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (1, 'member1', 'member1@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (2, 'member2', 'member2@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (3, 'member3', 'member3@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (4, 'member4', 'member4@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (5, 'member5', 'member5@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (6, 'member6', 'member6@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (7, 'member7', 'member7@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (8, 'member8', 'member8@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+insert into member(id, name, email, password, role_type, created_at, modified_at, created_by, modified_by)
+values (9, 'member9', 'member9@test.com', 'encrypted_password', 'CUSTOMER', '2024-08-27', null, null, null);
+
+insert into store(id, name, member_id, category, max_waiting_count, description, created_at, modified_at, created_by, modified_by)
+values (1, 'storeA', 1, 'SNACK_FOOD', 5, 'description', '2024-08-27', null, null, null);
